4c33df21493f6bc90f37e2aeccc1a62cdc9d4abe
Amanda <amanda.huiyingli@gmail.com>
2016-02-13 16:58:19 +0800

    add another codition when the list starts with a smaller number

--- test1.py
+++ test1.py
@@ -19,37 +19,56 @@ def numbers_in_lists(string):
     outer=[]
     for s in string:
         num_list.append(int(s))
-    while i<=len(string)+1:
-        while num_list[i]>=num_list[n]:
-            if i==n-1:
-                outer.append(num_list[i])
-                print outer
-            inner.append(num_list[n])
-            print inner
-            n=n+1
-            if n==len(string):break
-            print n
-        print inner
-        outer.append(inner)
-        print outer
-        i=i+n
-        n=i+1
-        if n==len(string)+1:break
-        print i
-        print n
-        inner=[]
+    
+    while i<=len(string)-1:
+        if num_list[i]<num_list[n]:
+          while num_list[i]<num_list[n]:
+              outer.append(num_list[i])
+              i=i+1
+              print i
+              n=n+1
+              print n
+              if n>=len(string):
+                  break
+          if len(outer)==len(string)-1:
+             outer.append(num_list[n-1])
+          print outer
+        if n>=len(string):break
+        print "end of the process 1"
+        if num_list[i]>=num_list[n]:
+          while num_list[i]>=num_list[n]:
+              if i==n-1:
+                  outer.append(num_list[i])
+                  print (outer)
+              inner.append(num_list[n])
+              print 'while2 inner:',inner
+              n=n+1
+              if n>=len(string):break
+              print 'while2 n',n
+          print 'if2 inner',inner
+          outer.append(inner)
+          print 'if2 outer', outer
+          i=n
+          n=i+1
+          print 'if2 i',i
+          print 'if2 n', n
+          if n>=len(string):break
+          inner=[]
     return outer
 #testcases
 string = '543987'
 numbers_in_lists(string)
 #result = [5,[4,3],9,[8,7]]
 #print repr(string), numbers_in_lists(string) == result
-#string= '987654321'
+string= '987654321'
+numbers_in_lists(string)
 #result = [9,[8,7,6,5,4,3,2,1]]
 #print repr(string), numbers_in_lists(string) == result
-#string = '455532123266'
+string = '455532123266'
+numbers_in_lists(string)
 #result = [4, 5, [5, 5, 3, 2, 1, 2, 3, 2], 6, [6]]
 #print repr(string), numbers_in_lists(string) == result
-#string = '123456789'
+string = '123456789'
+numbers_in_lists(string)
 #result = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 #print repr(string), numbers_in_lists(string) == result
